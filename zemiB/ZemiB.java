36f244c36f0b1d627eea914a930701a175407822
sntea <sntea-hogex@users.noreply.github.com>
2018-01-05 11:54:15 +0900

    iroiro

--- zemiB/ZemiB.java
+++ zemiB/ZemiB.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,6 +26,10 @@ class Trie {
 	
 	public boolean isContain(List<Integer> s) {
 		return nodes.get(0).isContain(s, 0);
+	}
+	
+	public void debug() {
+		
 	}
 }
 
@@ -67,60 +72,98 @@ class Node {
 	}
 }
 
+// ビームサーチのノード
+class BNode {
+	static final int height = 30;
+	static final int width = 30;
+	// このdx, dyで方向に番号をつける
+	// たとえば (-1, 0)方向は0
+	static final int dx[] = {-1, 0, 1, 0};
+	static final int dy[] = {0, -1, 0, 1};
+	// boolean[x][y][方向の番号] ですでに使った辺を管理する
+	boolean[][][] usedFlag;
+	
+	
+	public BNode() {
+		usedFlag = new boolean[height][width][4];
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				Arrays.fill(usedFlag[i][j], false);
+			}
+		}
+	}
+	
+	public BNode(boolean[][][] usedFlag) {
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				this.usedFlag[i][j] = usedFlag[i][j].clone();
+			}
+		}
+	}
+	
+	public int eval() {
+		// 評価値を返す
+		return -1;
+	}
+	
+	public List<BNode> generateNext() {
+		// 次のステップのノードを返す
+		List<BNode> ret = new ArrayList<BNode>();
+		// hogehoge
+		return ret;
+	}
+}
+
 public class ZemiB {
 
-
-	public static void main(final String[] args) {
-		try{
-			File file = new File("input.txt");
-			Scanner scan = new Scanner(file);
-			Trie trie = new Trie();
-			int n = Integer.parseInt(scan.next());
-			for(int i = 0; i < n; i++) {
-				int m = Integer.parseInt(scan.next());
-				List<Integer> tmp = new ArrayList<Integer>();
-				for(int j = 0; j < m; j++) {
-					tmp.add(Integer.parseInt(scan.next()));
-				}
-				System.out.println(tmp);
-				trie.add(tmp);
-			}
-			int k = Integer.parseInt(scan.next());
-			for(int i = 0; i < k; i++) {
-				int m = Integer.parseInt(scan.next());
-				List<Integer> tmp = new ArrayList<Integer>();
-				for(int j = 0; j < m; j++) {
-					tmp.add(Integer.parseInt(scan.next()));
-				}
-				System.out.println(trie.isContain(tmp));
-			}
-		}catch(FileNotFoundException e){
-			System.out.println(e);
-		}
+	
+	static final int dx[] = {-1, 0, 1, 0};
+	static final int dy[] = {0, -1, 0, 1};
+	final int height = 30;
+	final int width = 30;
+	int[][] field;
+	final int NONE = -1;
+	
+	public ZemiB() {
+		field = new int[height][width];
+	}
+	
+	
+	
+	public static void main(final String[] args) throws FileNotFoundException {
+		(new ZemiB()).run("input.txt");
 	}
 
 
-	public void run(final String inputFileName){
-		try {
-			List<String> buffer = Files.readAllLines(Paths.get(inputFileName));
-
-			for(int i=0;i<buffer.size();i++) {
-				System.out.println(buffer.get(i));
-			}
-
-			int [][] toporogy = createTopology(20,20);
-
-			for(int i=0;i<400;i++) {
-				if(toporogy[39][i]==1) {
-					System.out.println(i);
-				}
-			}
-
-
-		} catch (IOException e) {
-			// TODO 自動生成された catch ブロック
-			e.printStackTrace();
+	public void run(final String inputFileName) throws FileNotFoundException{
+		for(int i = 0; i < height; i++) {
+			Arrays.fill(field[i], NONE);
 		}
+		File file = new File(inputFileName);
+		Scanner scan = new Scanner(file);
+		while(scan.hasNext()) {
+			int c = Integer.parseInt(scan.next());
+			int x = Integer.parseInt(scan.next());
+			int y = Integer.parseInt(scan.next());
+			c--; x--; y--;
+			field[x][y] = c;
+		}
+		BNode answer = search();
+		// anser.printLog();
+	}
+	
+	
+	private BNode search() {
+		final int BeamWidth = 100;
+		List<BNode> nodes = new ArrayList<BNode>();
+		nodes.add(new BNode());
+		boolean endFlag = true;
+		do {
+			// 探索
+			// nodesの各要素に関してgenerateNextやってその結果をnodesとまーじ
+			// そして枝刈り
+		}while(endFlag);
+		return nodes.get(0);
 	}
 
 
